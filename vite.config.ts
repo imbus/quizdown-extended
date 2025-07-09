262d197ef2014480ea85b703789c382fb95955b3
cz-lucas <46531925+cz-lucas@users.noreply.github.com>
2025-07-09 11:59:48 +0000

    Fixed credits

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
 import autoprefixer from 'autoprefixer'
+import pkg from './package.json' 
 
 export default defineConfig({
   plugins: [
@@ -19,5 +20,9 @@ export default defineConfig({
     postcss: {
       plugins: [autoprefixer()]
     }
-  }
+  },
+ define: {
+  __NAME__: `"${pkg.name}"`,
+  __VERSION__: `"${pkg.version}"`,
+ },
 })
